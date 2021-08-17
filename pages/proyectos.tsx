884e439d9ad5084f1aa8daae1fca38771e081e57
Daniel Tello <dantello.319@gmail.com>
2021-08-17 00:20:03 -0500

    se termino el componente de proyecto

--- pages/proyectos.tsx
+++ pages/proyectos.tsx
@@ -1,8 +1,21 @@
+import { CSSProperties } from "react";
 import Proyecto from "../components/Proyecto";
 import img from "../assets/images/mejores-practicas-diseno-web-es-1024x512.jpg";
 const Proyectos = () => {
+   const styles: CSSProperties = {
+      margin: "50px 0",
+      display: "flex",
+      flexWrap: "wrap",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: "30px",
+      width: "90%",
+   };
+
    return (
-      <div>
+      <div style={styles}>
+         <Proyecto img={img} />
+         <Proyecto img={img} />
          <Proyecto img={img} />
       </div>
    );
