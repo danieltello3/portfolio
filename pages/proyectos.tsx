f48d96ed18376ae1d334c585be6f1a1ec50f91c2
Daniel Tello <dantello.319@gmail.com>
2021-08-21 00:13:51 -0500

    se agrego curvas al footer

--- pages/proyectos.tsx
+++ pages/proyectos.tsx
@@ -1,20 +1,14 @@
 import { CSSProperties } from "react";
 import Proyecto from "../components/Proyecto";
 import img from "../assets/images/mejores-practicas-diseno-web-es-1024x512.jpg";
+import styles from "../styles/Proyects.module.scss";
 const Proyectos = () => {
-   const styles: CSSProperties = {
-      margin: "50px 0",
-      display: "flex",
-      flexWrap: "wrap",
-      alignItems: "center",
-      justifyContent: "center",
-      gap: "30px",
-      width: "90%",
-      alignSelf: "center",
-   };
-
    return (
-      <div style={styles}>
+      <div className={styles.main}>
+         <div className={styles.title}>
+            <p>PROYECTOS</p>
+            <div></div>
+         </div>
          <Proyecto img={img} />
          <Proyecto img={img} />
          <Proyecto img={img} />
