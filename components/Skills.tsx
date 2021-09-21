4eebddf259f67588498e380deabedf17b3145a8b
Daniel Tello <dantello.319@gmail.com>
2021-09-21 01:24:37 -0500

    se arreglo skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,20 +2,20 @@ import Image from "next/image";
 import skillStyle from "../styles/Skills.module.scss";
 
 const skills = [
-   { img: "/skills/html.png", alt: "Html" },
-   { img: "/skills/Css.png", alt: "Css" },
-   { img: "/skills/javascript-escudo.png", alt: "Javascript" },
-   { img: "/skills/typescript.png", alt: "Typescript" },
-   { img: "/skills/React.png", alt: "React" },
-   { img: "/skills/nextjs.png", alt: "NextJs" },
-   { img: "/skills/python.png", alt: "Python" },
-   { img: "/skills/Git.png", alt: "Git" },
-   { img: "/skills/Django.png", alt: "Django" },
-   { img: "/skills/flask.png", alt: "Flask" },
-   { img: "/skills/nodejs.png", alt: "NodeJS" },
-   { img: "/skills/postgresql.png", alt: "PostgreSQL" },
-   { img: "/skills/MongoDB.png", alt: "MongoDB" },
-   { img: "/skills/expressjs.png", alt: "ExpressJS" },
+   { img: "/html.png", alt: "Html" },
+   { img: "/Css.png", alt: "Css" },
+   { img: "/javascript-escudo.png", alt: "Javascript" },
+   { img: "/typescript.png", alt: "Typescript" },
+   { img: "/React.png", alt: "React" },
+   { img: "/nextjs.png", alt: "NextJs" },
+   { img: "/python.png", alt: "Python" },
+   { img: "/Git.png", alt: "Git" },
+   { img: "/Django.png", alt: "Django" },
+   { img: "/flask.png", alt: "Flask" },
+   { img: "/nodejs.png", alt: "NodeJS" },
+   { img: "/postgresql.png", alt: "PostgreSQL" },
+   { img: "/MongoDB.png", alt: "MongoDB" },
+   { img: "/expressjs.png", alt: "ExpressJS" },
 ];
 
 const Skills = () => {
