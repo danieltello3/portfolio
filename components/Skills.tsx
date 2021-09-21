af6ee047c1971072f413297955fef9c675705c12
Daniel Tello <dantello.319@gmail.com>
2021-09-21 01:20:38 -0500

    se cambio ruta de imagenes en skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,35 +1,21 @@
 import Image from "next/image";
 import skillStyle from "../styles/Skills.module.scss";
-import react from "../public/skills/React.png";
-import git from "../public/skills/Git.png";
-import css from "../public/skills/Css.png";
-import django from "../public/skills/Django.png";
-import flask from "../public/skills/flask.png";
-import html from "../public/skills/html.png";
-import javascript from "../public/skills/javascript-escudo.png";
-import mongodb from "../public/skills/MongoDB.png";
-import nodejs from "../public/skills/nodejs.png";
-import postgres from "../public/skills/postgresql.png";
-import python from "../public/skills/python.png";
-import typescript from "../public/skills/typescript.png";
-import express from "../public/skills/expressjs.png";
-import nextjs from "../public/skills/nextjs.png";
 
 const skills = [
-   { img: html, alt: "Html" },
-   { img: css, alt: "Css" },
-   { img: javascript, alt: "Javascript" },
-   { img: typescript, alt: "Typescript" },
-   { img: react, alt: "React" },
-   { img: nextjs, alt: "NextJs" },
-   { img: python, alt: "Python" },
-   { img: git, alt: "Git" },
-   { img: django, alt: "Django" },
-   { img: flask, alt: "Flask" },
-   { img: nodejs, alt: "NodeJS" },
-   { img: postgres, alt: "PostgreSQL" },
-   { img: mongodb, alt: "MongoDB" },
-   { img: express, alt: "ExpressJS" },
+   { img: "/skills/html.png", alt: "Html" },
+   { img: "/skills/Css.png", alt: "Css" },
+   { img: "/skills/javascript-escudo.png", alt: "Javascript" },
+   { img: "/skills/typescript.png", alt: "Typescript" },
+   { img: "/skills/React.png", alt: "React" },
+   { img: "/skills/nextjs.png", alt: "NextJs" },
+   { img: "/skills/python.png", alt: "Python" },
+   { img: "/skills/Git.png", alt: "Git" },
+   { img: "/skills/Django.png", alt: "Django" },
+   { img: "/skills/flask.png", alt: "Flask" },
+   { img: "/skills/nodejs.png", alt: "NodeJS" },
+   { img: "/skills/postgresql.png", alt: "PostgreSQL" },
+   { img: "/skills/MongoDB.png", alt: "MongoDB" },
+   { img: "/skills/expressjs.png", alt: "ExpressJS" },
 ];
 
 const Skills = () => {
