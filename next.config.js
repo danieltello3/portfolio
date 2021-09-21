24649250585a1f99e3674f24d3f7c7068ee1a386
Daniel Tello <dantello.319@gmail.com>
2021-09-21 00:48:24 -0500

    solucion webpack al importar avatar

--- next.config.js
+++ next.config.js
@@ -7,4 +7,14 @@ module.exports = {
    env: {
       BACKEND_URL: process.env.BACKEND_URL,
    },
+   webpack: (config, { isServer }) => {
+      if (!isServer) {
+         // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+         config.resolve.fallback = {
+            fs: false,
+         };
+      }
+
+      return config;
+   },
 };
